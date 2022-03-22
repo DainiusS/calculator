33e671223f7bbf19f4aa97f63f0a4e2c606a9809
Dainius <dainius.skrivelis@gmail.com>
2022-03-22 11:20:45 +0200

    Added percentage, +/-, backspace on the keyboard

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const display = document.querySelector("#display");
 const buttons = document.querySelectorAll(".button");
 const operationButtons = document.querySelectorAll(".operation-button");
 const outcome = document.querySelector("#result");
-
+const percent = document.querySelector("#percent");
+const plusMinus = document.querySelector("#plus-minus");
 const ac = document.querySelector("#ac");
 const point = document.querySelector("#point");
 
@@ -10,12 +11,8 @@ const point = document.querySelector("#point");
 let firstOperand = "";
 let secondOperand = "";
 let operator;
-
 let createdNum = "";
-
-display.textContent = 0;
-
-
+let tempResult = "";
 
 buttons.forEach(btn => {
     btn.addEventListener("click", () => {
@@ -28,10 +25,11 @@ buttons.forEach(btn => {
 
 operationButtons.forEach(opBtn => {
     opBtn.addEventListener("click", () => {
+        tempResult = "";
         operator = opBtn.dataset.value;
         if (firstOperand == "") return;
         secondOperand = firstOperand;
-        display.textContent = secondOperand;
+        display.textContent = firstOperand;
         firstOperand = "";
     });
 });
@@ -40,13 +38,20 @@ function getResult (b, a, c){
     if (c === "+") return Number(b) + Number(a);
     if (c === "-") return Number(b) - Number(a);
     if (c === "*") return Number(b) * Number(a);
-    if (c === "/") return Number(b) / Number(a);
+    if (c === "/") {
+        if (a == 0){
+            return display.textContent = "LOL :)";
+        } else {
+            return Number(b) / Number(a);
+        }
+    }
 };
 
 outcome.addEventListener("click", () => {
-    let tempResult = getResult(secondOperand, firstOperand, operator);
-    display.textContent = tempResult;
+    if (tempResult !== "") return;
+    tempResult = getResult(secondOperand, firstOperand, operator);
     firstOperand = tempResult;
+    display.textContent = firstOperand;    
 });
 
 ac.addEventListener("click", () => {
@@ -54,6 +59,7 @@ ac.addEventListener("click", () => {
     secondOperand = "";
     operator = "";
     createdNum = "";
+    tempResult = "";
     display.textContent = 0;
 });
 
@@ -63,3 +69,36 @@ point.addEventListener("click", () => {
     firstOperand = firstOperand + createdNum;
     display.textContent = firstOperand;
 });
+
+plusMinus.addEventListener("click", () => {
+    firstOperand = -(firstOperand);
+    display.textContent = firstOperand;
+});
+
+function percentage(a, b) {
+    return Number(a) * Number(b) / 100;
+}
+
+percent.addEventListener("click", () => {
+    if (tempResult !== "") return;
+    operator = "";
+    tempResult = percentage(firstOperand, secondOperand);
+    firstOperand = tempResult;
+    display.textContent = firstOperand;
+});
+
+
+document.addEventListener("keydown", (e) => {
+    if (e.code == "Backspace"){
+        if (firstOperand.length >= 2){
+            let newFirstOperand = firstOperand.slice(0, -1);
+            firstOperand = newFirstOperand;
+            display.textContent = firstOperand;
+        } else if (firstOperand.length = 1) {
+            firstOperand = 0;
+            display.textContent = firstOperand;;
+        };
+    };
+});
+
+//round numbers, fix bugs, enable keyboard
